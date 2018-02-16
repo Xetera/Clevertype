a22ce9c966ba03c11f2e7cb192fc472d6505ed14
Ali <mythologyali@gmail.com>
2018-02-16 05:52:48 -0800

    removed js files

--- test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Cleverbot_1 = require("./Cleverbot");
-var cb = new Cleverbot_1.default('CC6zaMgw6Tvbd4_vrPSgtOhCM1Q');
-cb.say('Hey!').then(function (response) {
-    console.log(response);
-});
