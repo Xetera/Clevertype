6a4d1221ee99f0ad31ee2fc14e09c54c19800a27
Ali <mythologyali@gmail.com>
2018-02-17 15:15:59 -0800

    typings

---
+++ index.d.ts
@@ -0,0 +1,69 @@
+declare module "Cleverbot" {
+
+    export type CleverbotState = string;
+    export type Response = string;
+
+    export interface Mood {
+        emotion : number;
+        engagement : number;
+        regard : number;
+    }
+
+    export interface Config {
+        apiKey : string;
+        cs ?: CleverbotState;
+        mood : Mood;
+    }
+
+    export interface ChatHistory {
+        conversation ?: {
+            self: string;
+            cleverbot: string;
+        }
+    }
+
+    export interface APIResponse {
+        cs : CleverbotState;
+        interaction_count: number;
+        input: string;
+        output: string;
+        time_taken: number;
+        random_number: number;
+        time_second: number;
+        time_minute: number;
+        time_hour: number;
+        time_day_of_week: number;
+        time_day: number;
+        time_month: number;
+        time_year: number;
+    }
+
+
+    export class Client {
+        private endpoint : string;
+        private config : Config;
+        private CleverbotState : CleverbotState;
+        private numberOfAPICalls : number;
+        private wrapperName : string;
+
+        private readonly encodedWrapperName: string;
+        private readonly encodedEmotion : string;
+        private readonly encodedEngagement : string;
+        private readonly encodedRegard : string;
+        private readonly encodedCleverbotState : string;
+
+        public readonly callAmount : number;
+        public readonly mood : Mood;
+
+        public constructor(apiKey: string);
+        private static encodeInput(input : string ) : string;
+        private retrieveCsEndpoint() : CleverbotState;
+        private setCleverbotState(input : string) : void;
+
+        public setEmotion(amount : number) : void;
+        public setEngagement(amount : number) : void;
+        public setRegard(amount : number) : void;
+        public say(message : string): Promise<Response>;
+
+    }
+}
