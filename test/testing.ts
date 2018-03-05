73dce3198eb9544a36ce44567055ee52c5db5cff
Ali <mythologyali@gmail.com>
2018-03-04 21:26:20 -0800

    added more testing

--- test/testing.ts
+++ test/testing.ts
@@ -3,22 +3,60 @@ import 'mocha'
 import {Cleverbot} from '../Cleverbot'
 const testingKey = require('../config.json').key;
 
-let cleverbot : Cleverbot = new Cleverbot("2323");
+let cleverbot : Cleverbot;
 
-let testResponse2: string;
-
-describe('Inputting wrong api key', () => {
-    it('Should throw an error', () => {
-        let test : Cleverbot = new Cleverbot("2323");
-        return expect(Promise.resolve(test.say('test'))).throw('Error');
+describe('Invoking constructor', () => {
+    it('Correct API key should work as intended', done => {
+        expect(function(){
+            cleverbot = new Cleverbot(testingKey);
+            done();
+        }).to.not.throw(Error);
     })
 });
 
-describe('Sending successful requests', () => {
-    it('Should return a string', () => {
-        cleverbot.say('❤').then(response => {
-            console.log(response);
-            expect(response).to.be.a('string').and.not.equal('');
+describe('Sending requests', function () {
+    this.timeout(3000);
+    it('Returning a string and incrementing message count.', function(done) {
+        this.timeout(3000);
+        cleverbot.say('send me an emoji please').then(response => {
+            expect(response).to.be.a('string');
+        }).then(() => {
+            expect(cleverbot.callAmount).to.equal(1);
+            done();
         });
     });
+});
+
+describe('Cleverbot Moods', () => {
+    it('Setting Moods out of bounds should throw errors', () => {
+        expect(function() {
+            cleverbot.setRegard(101)
+        }).to.throw(RangeError, "Regard must be a value between 0 and 100.");
+        expect(function() {
+            cleverbot.setEmotion(101)
+        }).to.throw(RangeError, "Emotion must be a value between 0 and 100.");
+        expect(function(){
+            cleverbot.setEngagement(101)
+        }).to.throw(RangeError, "Engagement must be a value between 0 and 100.");
+    });
+
+    it('Engagement should be calling without errors', () => {
+        expect(function(){
+            cleverbot.setEmotion(20)
+        }).to.not.throw(RangeError);
+        expect(function(){
+            cleverbot.setRegard(20)
+        }).to.not.throw(RangeError);
+        expect(function(){
+            cleverbot.setEngagement(20)
+        }).to.not.throw(RangeError);
+    });
+
+    it('Cleverbot moods should be setting properly', () => {
+        expect(cleverbot.mood).to.deep.equal({
+            emotion:20,
+            engagement:20,
+            regard:20
+        });
+    })
 });
