1577928d42986c971f4adb9b8eece50247e988ae
Ali <mythologyali@gmail.com>
2018-03-16 13:15:54 -0700

    exceptions moved to its own file

---
+++ Exceptions.ts
@@ -0,0 +1,9 @@
+export const Exceptions : { [index:string] : string } = {
+    '401': 'Invalid API Key',
+    '404': 'API Was not found',
+    '413': 'The request was too large (over 64kb)',
+    '414': 'The request was too large (over 64kb)',
+    '502': 'Could not get a reply from Cleverbot API Servers',
+    '503': 'Too many requests',
+    '504': 'Could not get a reply from Cleverbot API Servers',
+};
