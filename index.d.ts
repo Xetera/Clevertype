3a019ab5a638034e7221a35732531a6849658c32
Ali <mythologyali@gmail.com>
2018-03-16 13:16:42 -0700

    no implicit any and type declaration updates

--- index.d.ts
+++ index.d.ts
@@ -1,76 +1,70 @@
 declare module "clevertype" {
-
     export type CleverbotState = string;
-    export type Response = string;
 
     export interface Mood {
-        emotion : number;
-        engagement : number;
-        regard : number;
+        emotion ?: number;
+        engagement ?: number;
+        regard ?: number;
     }
 
     export interface Config {
         apiKey : string;
         cs ?: CleverbotState;
         mood : Mood;
-        debug: boolean;
     }
 
+    export type UserInput = string;
+    export type CleverbotResponse = string;
+
+    export type Interaction  = [UserInput, CleverbotResponse];
     export interface ChatHistory {
-        conversation ?: {
-            self: string;
-            cleverbot: string;
-        }
+        input: UserInput;
+        response: CleverbotResponse;
+        number : number;
+        mood: Mood;
+        requestDate: Date;
+        responseDate: Date;
+        delay: number;
+        getConversation(): Interaction
     }
 
-    export interface User {
-        id: string;
-        mood: Mood;
-        history?:ChatHistory;
-        cs?: CleverbotState;
-    }
+
 
     export interface APIResponse {
         cs : CleverbotState;
-        interaction_count: number;
-        input: string;
+        interaction_count: string;
         output: string;
-        time_taken: number;
-        random_number: number;
-        time_second: number;
-        time_minute: number;
-        time_hour: number;
-        time_day_of_week: number;
-        time_day: number;
-        time_month: number;
-        time_year: number;
+        input: string;
+        input_other: string;
+        predicted_input: string;
+        filtered_input: string;
+        filtered_input_other: string;
+        input_label: string;
+        emotion: string;
+        emotion_tone: string;
+        emotion_values: string;
+        accuracy: string;
+        conversation_id: string;
+        database_version: string;
+        errorline: string;
+        reaction: string;
+        reaction_tone: string;
+        reaction_values: string;
+        CSRES30: string;
+        clever_accuracy: string;
+        clever_output: string;
+        clever_match: string;
+        callback: string;
+        time_elapsed: string;
+        time_taken: string;
+        random_number: string;
+        time_second: string;
+        time_minute: string;
+        time_hour: string;
+        time_day_of_week: string;
+        time_day: string;
+        time_month: string;
+        time_year: string;
     }
 
-    export class Cleverbot {
-        private endpoint : string;
-        private config : Config;
-        private CleverbotState : CleverbotState;
-        private numberOfAPICalls : number;
-        private wrapperName : string;
-
-        private readonly encodedWrapperName: string;
-        private readonly encodedEmotion : string;
-        private readonly encodedEngagement : string;
-        private readonly encodedRegard : string;
-        private readonly encodedCleverbotState : string;
-
-        public readonly callAmount : number;
-        public readonly mood : Mood;
-
-        public constructor(apiKey: string | Config);
-        private static encodeInput(input : string ) : string;
-        private retrieveCsEndpoint() : CleverbotState;
-        private setCleverbotState(input : string) : void;
-
-        public setEmotion(amount : number) : void;
-        public setEngagement(amount : number) : void;
-        public setRegard(amount : number) : void;
-        public say(message : string): Promise<Response>;
-
-    }
 }
